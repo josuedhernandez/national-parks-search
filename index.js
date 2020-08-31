4fdf3ac32995609e1363da0425ed23cd8c772acb
Josue <52310147+josuedhernandez@users.noreply.github.com>
2020-08-31 16:39:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function getParks(query, maxResults = 5) {
     limit: maxResults
   };
   const queryString = formatQueryParams(params);
-  const url = searchURL + "?" + queryString + "&api_key=TyyqlQDFmTZTbVKPauKderycw5qLeX0VYlVvSdta";
+  const url = searchURL + "?" + queryString + `&api_key=${apiKey}`;
 
   console.log(url);
 
